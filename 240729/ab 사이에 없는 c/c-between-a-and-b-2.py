09ff78aa75ac8fdad518ec47b7e3b157cb5bc98a
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-07-29 16:58:07 +0900

    Title: ab 사이에 없는 c, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,14 @@
+k=input()
+arr=k.split()
+a=int(arr[0])
+b=int(arr[1])
+c=int(arr[2])
+con=False
+for i in range(a,b+1):
+    if c%i==0:
+        con=True
+        break
+if con==False:
+    print("YES")
+else:
+    print("NO")
