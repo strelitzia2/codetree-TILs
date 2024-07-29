9c5fb5356b049823c8dcd57d0956cbd3d401aa1b
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-07-29 16:43:34 +0900

    Title: 공약수의 유무, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240729/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -3,11 +3,11 @@ arr=k.split()
 a=int(arr[0])
 b=int(arr[1])
 c=False
-for i in range(2,a):
-    if a%i==0 and b%i==0:
+for i in range(a,b+1):
+    if 1920%i==0 and 2880%i==0:
         c=True
         break
 if c==True:
     print(1)
-elif c==False:
+else:
     print(0)
