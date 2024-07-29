2128c83bb9a7b866f14040ab593c6d3c5693406d
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-07-29 13:39:56 +0900

    Title: 3n + 1 수열, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,13 @@
+e=int(input())
+count=0
+while True:
+    if e==1:
+        count+=1
+        break
+    elif e%2==0:
+        e=e//2
+        count+=1
+    else:
+        e=e*3+1
+        count+=1
+print(count)
