c74b5568422310e3257fdcd37a20ee58cc758676
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-01 17:24:22 +0900

    Title: 별표 출력하기 6, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240801/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range((2*n)-(2-i)-1):
+        print("*",end=" ")
+    print()
+for i in range(n-2,-1,-1):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range((2*n)-(2-i)-1):
+        print("*",end=" ")
+    print()
