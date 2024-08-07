dd082fe219e000698a2b10f2ec2bff2ef6de25ff
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-07 13:01:52 +0900

    Title: 체크 다이아몬드, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240807/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(1,n):
+    for j in range(n-i):
+        print("",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print()
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print("",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print()
