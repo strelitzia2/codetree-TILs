31e0d7fc259fc400d1fcbacd6e0c0d3851187f20
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-12 15:33:42 +0900

    Title: 행과 열, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/행과 열/row-column.py
+++ 240812/행과 열/row-column.py
@@ -1,8 +1,8 @@
 n=input()
 arr=n.split(" ")
-a=arr[0]
-b=arr[1]
-for i in range(1,5):
-    for j in range(1,6):
+a=int(arr[0])
+b=int(arr[1])
+for i in range(1,a+1):
+    for j in range(1,b+1):
         print(i*j,end=" ")
     print()
