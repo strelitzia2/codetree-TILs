cb5e881ed912b6a4df2387706b8b1219a5a56add
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-12 11:21:53 +0900

    Title: 규칙을 찾아 출력하기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240812/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -4,8 +4,8 @@ for i in range(n):
         if n==1:
             print("*",end=" ")
         else:
-            if (i and j)!=0 and (i and j)!=n-1 and i<j:
+            if (i and j)!=0 and (i and j)!=n-1 and i<=j:
                 print(" ",end=" ")
             else:
                 print("*",end=" ")
-        print()
+    print()
