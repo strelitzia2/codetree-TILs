c8a78eecc045f8f37c4b581eb44caa33e86a1958
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-12 10:57:50 +0900

    Title: 별표 출력하기 12, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240812/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,14 @@
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if j%2==0:
+            if i==0:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+        else:
+            if i<=j:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+    print()
