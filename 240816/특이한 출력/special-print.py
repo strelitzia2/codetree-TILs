07ba93ef4e1f25fff3a984cccac6a6adbb13dfa6
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-16 13:31:50 +0900

    Title: 특이한 출력, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240816/특이한 출력/special-print.py
@@ -0,0 +1,7 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if (i+j)%4==0:
+            print(f"{i, j}",end="\n")
+        else:
+            print(f"{i, j}",end=" ")
