3d264ab11f1685efa7fe937abab241deb7ea3fd3
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-21 13:45:22 +0900

    Title: 알파벳 출력하기 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240821/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,11 @@
+n=int(input())
+cnt="A"
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(n-i-1):
+        print(cnt,end=" ")
+        cnt=chr(ord(cnt)+1)
+        if cnt>"Z":
+            cnt="A"
+    print()
