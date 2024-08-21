9eca37d35c468a0b53cebdc4cc65031f25c6bd7e
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-21 13:46:02 +0900

    Title: 알파벳 출력하기 2, Time: 163ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/알파벳 출력하기 2/print-alphabet-2.py
+++ 240821/알파벳 출력하기 2/print-alphabet-2.py
@@ -1,9 +1,9 @@
 n=int(input())
 cnt="A"
 for i in range(n):
-    for j in range(i+1):
+    for j in range(i):
         print(" ",end=" ")
-    for j in range(n-i-1):
+    for j in range(n-i):
         print(cnt,end=" ")
         cnt=chr(ord(cnt)+1)
         if cnt>"Z":
