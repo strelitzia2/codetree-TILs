7da9b3ec080ec02e01b092c1e749b8078ed5fedf
Chong Taedu <89844413+strelitzia2@users.noreply.github.com>
2024-08-26 16:43:19 +0900

    Title: n개의 숫자 중 최소, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240826/n개의 숫자 중 최소/min-of-n-num.py
+++ 240826/n개의 숫자 중 최소/min-of-n-num.py
@@ -1,6 +1,11 @@
+k=int(input())
 arr=(list(map(int,input().split())))
 minv=arr[0]
+cnt=1
 for elem in arr:
     if elem<minv:
         minv=elem
-print(minv)
+        cnt=1
+    elif minv==elem:
+        cnt+=1
+print(minv,cnt)
